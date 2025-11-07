94a67302b4d196622454dadae1fb35b5c55f6448
Joel Rdz <joelrdzh@gmail.com>
2025-11-07 11:52:29 -0600

    Aura Spa Menus and corrections

---
+++ _next/static/H7ypFjnE9SUDosZBEhTDw/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/52f0880312e41919.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
